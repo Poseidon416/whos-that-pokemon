f9de137c597cf6194d908c78009f5897df886c79
Nikhil Mantha <nikhilmantha@nikhils-mbp.lan>
2024-08-02 23:53:00 -0700

    Aug 2

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from app import app
+import pandas as pd
+
+def convert_meters(meters):
+    inches = round(meters * 39.37)
+    feet, inches = divmod(inches, 12)
+    return f'{feet}\' {inches}"'
+
+def convert_kgs(kgs):
+    return round((kgs*2.205), 1)
+
+stats = pd.read_csv('app/static/pokedex/stats.csv')
+stats.set_index('Name')
 
